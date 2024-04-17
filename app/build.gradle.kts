8daa09f13570f8934ce6a836e0c04a48a0eda6ed
mhhpc <hamidimhhossein@gmail.com>
2024-04-18 01:13:01 +0330

    some changes on main_activity and themes and styles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,23 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //animation
+    implementation("com.andkulikov:transitionseverywhere:1.7.8")
+    //lottie
+    implementation("com.airbnb.android:lottie:5.2.0")
+    //circle image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    //shape and view
+    implementation("io.github.florent37:shapeofview:1.4.7")
+    //picasso (ImageLoader)
+    implementation("com.squareup.picasso:picasso:2.71828")
+    //custom toast
+    implementation("io.github.muddz:styleabletoast:2.4.0")
 }
--- app/src/main/java/com/android/mylibrary/MainActivity.java
+++ app/src/main/java/com/android/mylibrary/MainActivity.java
@@ -1,14 +1,84 @@
 package com.android.mylibrary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.transitionseverywhere.Transition;
+import com.transitionseverywhere.TransitionManager;
+import com.transitionseverywhere.extra.Scale;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Gradient -----------
+        drawerLayout = findViewById(R.id.main);
+        AnimationDrawable animationDrawable = (AnimationDrawable) drawerLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+        //first run dialog --------------
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart", true);
+        if (firstStart) {
+            showStartDialog();
+        }
+
+        //Animation ------------
+        final ViewGroup r = findViewById(R.id.main);
+        final LinearLayout t1 = findViewById(R.id.list);
+        final LinearLayout t2 = findViewById(R.id.menu1);
+        final LinearLayout t3 = findViewById(R.id.list2);
+        final LinearLayout t4 = findViewById(R.id.ll1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Transition transition = new Scale();
+                transition.setDuration(600);
+                TransitionManager.beginDelayedTransition(r, transition);
+                t1.setVisibility(View.VISIBLE);
+                t2.setVisibility(View.VISIBLE);
+                t3.setVisibility(View.VISIBLE);
+                t4.setVisibility(View.VISIBLE);
+            }
+        }, 200);
+
     }
+
+    //first run dialog ----------------------
+    private void showStartDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("توجه!")
+                .setMessage("هنگام ورود به هر قسمت از برنامه برای اولین بار، اینترنت خود را روشن نگه دارید تا بهترین تجربه را از کتابخانه من داشته باشید (:")
+                .setPositiveButton("تایید", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create().show();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
+    }
+
 }
