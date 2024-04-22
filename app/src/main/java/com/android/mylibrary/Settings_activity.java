e9c14a9413d4f7ffefc20849c20e26ab6f60adf5
mhhpc <hamidimhhossein@gmail.com>
2024-04-22 21:37:54 +0330

    make Settings_activity (Dark mode radio group) better add full splash screen

--- app/src/main/java/com/android/mylibrary/Settings_activity.java
+++ app/src/main/java/com/android/mylibrary/Settings_activity.java
@@ -1,40 +1,48 @@
 package com.android.mylibrary;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RadioGroup;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
 public class Settings_activity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private RadioGroup radioGroup;
+    private SharedPreferences sharedPreferences;
+    private static final String THEME_PREF = "theme_pref";
+    private static final String TAG = "SettingsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        MaterialButtonToggleGroup btg_theme = findViewById(R.id.btg_theme);
-        btg_theme.addOnButtonCheckedListener((group, selectedBtnId, isChecked) -> {
-            if (isChecked) {
-                int theme;
-                switch (selectedBtnId) {
-                    case R.id.btnDefault:
-                        theme = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
-                        break;
-                    case R.id.btnDark:
-                        theme = AppCompatDelegate.MODE_NIGHT_YES;
-                        break;
-                    default:
-                        theme = AppCompatDelegate.MODE_NIGHT_NO;
-                        break;
-                }
-                Log.d(TAG, "isChecked:" + isChecked + " theme:" + theme);
-                AppCompatDelegate.setDefaultNightMode(theme);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            int theme;
+            switch (checkedId) {
+                case R.id.btnLight:
+                    theme = AppCompatDelegate.MODE_NIGHT_NO;
+                    break;
+                case R.id.btnDark:
+                    theme = AppCompatDelegate.MODE_NIGHT_YES;
+                    break;
+                case R.id.btnDefault:
+                    theme = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+                    break;
+                default:
+                    theme = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+                    break;
             }
+            Log.d(TAG, "isChecked:" + (checkedId != -1) + " theme:" + theme);
+            AppCompatDelegate.setDefaultNightMode(theme);
         });
 
     }
---
+++ app/src/main/java/com/android/mylibrary/Splash_activity.java
@@ -0,0 +1,26 @@
+package com.android.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Go to Home activity -----------
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_activity.this, MainActivity.class));
+                finish();
+            }
+        },1000);
+
+    }
+}
