13e30f33b4e609d73472db53f4e027254bd884d5
MHH <hamidimhhossein@gmail.com>
2024-10-18 02:42:41 +0330

    Complete Cat_emotional ...

--- app/src/main/java/com/android/mylibrary/Categories/Cat_emotional.java
+++ app/src/main/java/com/android/mylibrary/Categories/Cat_emotional.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,15 +22,68 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.core.widget.NestedScrollView;
 
 import com.android.mylibrary.R;
+import com.android.mylibrary.Stories.Sto10_activity;
+import com.android.mylibrary.Stories.Sto11_activity;
+import com.android.mylibrary.Stories.Sto12_activity;
+import com.android.mylibrary.Stories.Sto13_activity;
+import com.android.mylibrary.Stories.Sto14_activity;
+import com.android.mylibrary.Stories.Sto15_activity;
+import com.android.mylibrary.Stories.Sto16_activity;
+import com.android.mylibrary.Stories.Sto17_activity;
+import com.android.mylibrary.Stories.Sto18_activity;
+import com.android.mylibrary.Stories.Sto19_activity;
 import com.android.mylibrary.Stories.Sto1_activity;
+import com.android.mylibrary.Stories.Sto20_activity;
+import com.android.mylibrary.Stories.Sto21_activity;
+import com.android.mylibrary.Stories.Sto22_activity;
+import com.android.mylibrary.Stories.Sto23_activity;
+import com.android.mylibrary.Stories.Sto24_activity;
+import com.android.mylibrary.Stories.Sto25_activity;
+import com.android.mylibrary.Stories.Sto26_activity;
+import com.android.mylibrary.Stories.Sto27_activity;
+import com.android.mylibrary.Stories.Sto28_activity;
+import com.android.mylibrary.Stories.Sto29_activity;
 import com.android.mylibrary.Stories.Sto2_activity;
+import com.android.mylibrary.Stories.Sto30_activity;
+import com.android.mylibrary.Stories.Sto31_activity;
+import com.android.mylibrary.Stories.Sto32_activity;
+import com.android.mylibrary.Stories.Sto33_activity;
+import com.android.mylibrary.Stories.Sto34_activity;
+import com.android.mylibrary.Stories.Sto35_activity;
+import com.android.mylibrary.Stories.Sto36_activity;
+import com.android.mylibrary.Stories.Sto37_activity;
+import com.android.mylibrary.Stories.Sto38_activity;
+import com.android.mylibrary.Stories.Sto39_activity;
 import com.android.mylibrary.Stories.Sto3_activity;
+import com.android.mylibrary.Stories.Sto40_activity;
+import com.android.mylibrary.Stories.Sto41_activity;
+import com.android.mylibrary.Stories.Sto42_activity;
+import com.android.mylibrary.Stories.Sto43_activity;
+import com.android.mylibrary.Stories.Sto44_activity;
+import com.android.mylibrary.Stories.Sto45_activity;
+import com.android.mylibrary.Stories.Sto46_activity;
+import com.android.mylibrary.Stories.Sto47_activity;
+import com.android.mylibrary.Stories.Sto48_activity;
+import com.android.mylibrary.Stories.Sto49_activity;
 import com.android.mylibrary.Stories.Sto4_activity;
+import com.android.mylibrary.Stories.Sto50_activity;
+import com.android.mylibrary.Stories.Sto51_activity;
+import com.android.mylibrary.Stories.Sto52_activity;
+import com.android.mylibrary.Stories.Sto53_activity;
+import com.android.mylibrary.Stories.Sto54_activity;
+import com.android.mylibrary.Stories.Sto55_activity;
+import com.android.mylibrary.Stories.Sto56_activity;
+import com.android.mylibrary.Stories.Sto57_activity;
+import com.android.mylibrary.Stories.Sto58_activity;
+import com.android.mylibrary.Stories.Sto59_activity;
 import com.android.mylibrary.Stories.Sto5_activity;
+import com.android.mylibrary.Stories.Sto60_activity;
 import com.android.mylibrary.Stories.Sto6_activity;
 import com.android.mylibrary.Stories.Sto7_activity;
 import com.android.mylibrary.Stories.Sto8_activity;
+import com.android.mylibrary.Stories.Sto9_activity;
 import com.android.mylibrary.Stories_activity;
+import com.android.mylibrary.TextSize;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.squareup.picasso.Picasso;
 import com.transitionseverywhere.Slide;
@@ -38,10 +92,37 @@ import com.transitionseverywhere.TransitionManager;
 
 public class Cat_emotional extends AppCompatActivity {
 
+    private TextSize settingsManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_emotional);
+
+        //Change text size
+        settingsManager = new TextSize(this);
+        TextView Textview1 = findViewById(R.id.tv1);
+        TextView Textview4 = findViewById(R.id.tv4);
+        TextView Textview8 = findViewById(R.id.tv8);
+        TextView Textview12 = findViewById(R.id.tv12);
+        TextView Textview14 = findViewById(R.id.tv14);
+        TextView Textview25 = findViewById(R.id.tv25);
+        TextView Textview29 = findViewById(R.id.tv29);
+        TextView Textview32 = findViewById(R.id.tv32);
+        TextView Textview35 = findViewById(R.id.tv35);
+        TextView Textview46 = findViewById(R.id.tv46);
+
+        float textSize = settingsManager.getTextSize();
+        Textview1.setTextSize(textSize);
+        Textview4.setTextSize(textSize);
+        Textview8.setTextSize(textSize);
+        Textview12.setTextSize(textSize);
+        Textview14.setTextSize(textSize);
+        Textview25.setTextSize(textSize);
+        Textview29.setTextSize(textSize);
+        Textview32.setTextSize(textSize);
+        Textview35.setTextSize(textSize);
+        Textview46.setTextSize(textSize);
 
         //Animation ------------
         final ViewGroup r = findViewById(R.id.nested_scrollview);
@@ -49,6 +130,8 @@ public class Cat_emotional extends AppCompatActivity {
         final LinearLayout t2 = findViewById(R.id.menu2);
         final LinearLayout t3 = findViewById(R.id.menu3);
         final LinearLayout t4 = findViewById(R.id.menu4);
+        final LinearLayout t5 = findViewById(R.id.menu5);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -60,6 +143,7 @@ public class Cat_emotional extends AppCompatActivity {
                 t2.setVisibility(View.VISIBLE);
                 t3.setVisibility(View.VISIBLE);
                 t4.setVisibility(View.VISIBLE);
+                t5.setVisibility(View.VISIBLE);
             }
         }, 200);
 
@@ -89,32 +173,6 @@ public class Cat_emotional extends AppCompatActivity {
             }
         });
 
-        @SuppressLint("CutPasteId") CardView cv2 = (CardView) findViewById(R.id.mcv2);
-        cv2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv2), "trans1");
-                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv2), "trans2");
-
-                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
-
-                Intent intent = new Intent(Cat_emotional.this, Sto2_activity.class);
-                startActivity(intent, options.toBundle());
-            }
-        });
-
-        @SuppressLint("CutPasteId") CardView cv3 = (CardView) findViewById(R.id.mcv3);
-        cv3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv3), "trans1");
-                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv3), "trans2");
-
-                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
-
-                Intent intent = new Intent(Cat_emotional.this, Sto3_activity.class);
-                startActivity(intent, options.toBundle());
-            }
-        });
-
         @SuppressLint("CutPasteId") CardView cv4 = (CardView) findViewById(R.id.mcv4);
         cv4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -124,45 +182,6 @@ public class Cat_emotional extends AppCompatActivity {
                 ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
 
                 Intent intent = new Intent(Cat_emotional.this, Sto4_activity.class);
-                startActivity(intent, options.toBundle());
-            }
-        });
-
-        @SuppressLint("CutPasteId") CardView cv5 = (CardView) findViewById(R.id.mcv5);
-        cv5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv5), "trans1");
-                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv5), "trans2");
-
-                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
-
-                Intent intent = new Intent(Cat_emotional.this, Sto5_activity.class);
-                startActivity(intent, options.toBundle());
-            }
-        });
-
-        @SuppressLint("CutPasteId") CardView cv6 = (CardView) findViewById(R.id.mcv6);
-        cv6.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv6), "trans1");
-                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv6), "trans2");
-
-                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
-
-                Intent intent = new Intent(Cat_emotional.this, Sto6_activity.class);
-                startActivity(intent, options.toBundle());
-            }
-        });
-
-        @SuppressLint("CutPasteId") CardView cv7 = (CardView) findViewById(R.id.mcv7);
-        cv7.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv7), "trans1");
-                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv7), "trans2");
-
-                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
-
-                Intent intent = new Intent(Cat_emotional.this, Sto7_activity.class);
                 startActivity(intent, options.toBundle());
             }
         });
@@ -180,6 +199,97 @@ public class Cat_emotional extends AppCompatActivity {
             }
         });
 
+        @SuppressLint("CutPasteId") CardView cv12 = (CardView) findViewById(R.id.mcv12);
+        cv12.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv12), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv12), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto12_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        @SuppressLint("CutPasteId") CardView cv14 = (CardView) findViewById(R.id.mcv14);
+        cv14.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv14), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv14), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto14_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        @SuppressLint("CutPasteId") CardView cv25 = (CardView) findViewById(R.id.mcv25);
+        cv25.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv25), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv25), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto25_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        @SuppressLint("CutPasteId") CardView cv29 = (CardView) findViewById(R.id.mcv29);
+        cv29.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv29), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv29), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto29_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        @SuppressLint("CutPasteId") CardView cv32 = (CardView) findViewById(R.id.mcv32);
+        cv32.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv32), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv32), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto32_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        @SuppressLint("CutPasteId") CardView cv35 = (CardView) findViewById(R.id.mcv35);
+        cv35.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv35), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv35), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto35_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        @SuppressLint("CutPasteId") CardView cv46 = (CardView) findViewById(R.id.mcv46);
+        cv46.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pair<View, String> pair1 = Pair.create(findViewById(R.id.miv46), "trans1");
+                Pair<View, String> pair2 = Pair.create(findViewById(R.id.tv46), "trans2");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(Cat_emotional.this, pair1, pair2);
+
+                Intent intent = new Intent(Cat_emotional.this, Sto46_activity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
         //Image links
         ImageView imageView0 = (ImageView) findViewById(R.id.image);
         Picasso.get().load("https://s8.uupload.ir/files/open-book_auok.png").placeholder(R.drawable.logo2).into(imageView0);
@@ -187,26 +297,32 @@ public class Cat_emotional extends AppCompatActivity {
         ImageView imageView1 = (ImageView) findViewById(R.id.miv1);
         Picasso.get().load("https://s8.uupload.ir/files/360_f_688508974_o73n5n95kfabmllpq3kmqci5grlqkvnj_3oqo.jpg").placeholder(R.drawable.logo3).into(imageView1);
 
-        ImageView imageView2 = (ImageView) findViewById(R.id.miv2);
-        Picasso.get().load("https://s8.uupload.ir/files/ed2b1b0ad5e44bd58d05de6a8558b19e_djlo.jpg").placeholder(R.drawable.logo3).into(imageView2);
-
-        ImageView imageView3 = (ImageView) findViewById(R.id.miv3);
-        Picasso.get().load("https://s8.uupload.ir/files/choopan_5ssq.jpg").placeholder(R.drawable.logo3).into(imageView3);
-
         ImageView imageView4 = (ImageView) findViewById(R.id.miv4);
         Picasso.get().load("https://s8.uupload.ir/files/most-expensive-homes-042616-hero1_v0hj.jpg").placeholder(R.drawable.logo3).into(imageView4);
 
-        ImageView imageView5 = (ImageView) findViewById(R.id.miv5);
-        Picasso.get().load("https://s8.uupload.ir/files/f_-bus-invece-di-treni-bxyc_6ya4.jpg").placeholder(R.drawable.logo3).into(imageView5);
-
-        ImageView imageView6 = (ImageView) findViewById(R.id.miv6);
-        Picasso.get().load("https://s8.uupload.ir/files/jackal-symbolism-and-meaning-04a4efbd_3df4.jpg").placeholder(R.drawable.logo3).into(imageView6);
-
-        ImageView imageView7 = (ImageView) findViewById(R.id.miv7);
-        Picasso.get().load("https://s8.uupload.ir/files/alfred-bernhard-nobel-portrait-emil-osterman-foundation-1915_tkim.jpg").placeholder(R.drawable.logo3).into(imageView7);
-
         ImageView imageView8 = (ImageView) findViewById(R.id.miv8);
         Picasso.get().load("https://s8.uupload.ir/files/parenting-parents_7r0.jpg").placeholder(R.drawable.logo3).into(imageView8);
+
+        ImageView imageView12 = (ImageView) findViewById(R.id.miv12);
+        Picasso.get().load("https://s8.uupload.ir/files/jakav.com-ax-photo-tabiat-nature-tasavir-iran-ranginkaman_o1u6.jpg").placeholder(R.drawable.logo3).into(imageView12);
+
+        ImageView imageView14 = (ImageView) findViewById(R.id.miv14);
+        Picasso.get().load("https://s8.uupload.ir/files/roya_in9.jpg").placeholder(R.drawable.logo3).into(imageView14);
+
+        ImageView imageView25 = (ImageView) findViewById(R.id.miv25);
+        Picasso.get().load("https://s8.uupload.ir/files/circus-vancouver-1024x694_75u.jpg").placeholder(R.drawable.logo3).into(imageView25);
+
+        ImageView imageView29 = (ImageView) findViewById(R.id.miv29);
+        Picasso.get().load("https://s8.uupload.ir/files/5f0d7f5a19134_merh.jpg").placeholder(R.drawable.logo3).into(imageView29);
+
+        ImageView imageView32 = (ImageView) findViewById(R.id.miv32);
+        Picasso.get().load("https://s8.uupload.ir/files/whitepelican_edit_shadowlift_muhg.jpg").placeholder(R.drawable.logo3).into(imageView32);
+
+        ImageView imageView35 = (ImageView) findViewById(R.id.miv35);
+        Picasso.get().load("https://s8.uupload.ir/files/coffee-menu4_tkm2.jpg").placeholder(R.drawable.logo3).into(imageView35);
+
+        ImageView imageView46 = (ImageView) findViewById(R.id.miv46);
+        Picasso.get().load("https://s8.uupload.ir/files/8d3452e5e0956f2a97768276a3f09a4e-e1705487782263_wdy6.jpg").placeholder(R.drawable.logo3).into(imageView46);
 
     }
 }
